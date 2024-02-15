6524a0c64baca3380fb98734cd3b7128a40f3623
ganeshrm12 <ganeshmaharanawar@gmail.com>
2024-02-15 19:52:11 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ public class Main{
 
     public static void main(String [] args){
      
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
 
         System.out.println("HEllo World");
         System.out.println(new Date());
         System.out.println("Heyy");
         System.out.println("Heyy123");
         System.out.println("Heyy456");
-        System.out.println("Enter name : ");
-        String name = sc.nextLine();
+        //System.out.println("Enter name : ");
+        //String name = sc.nextLine();
         System.out.println(name);
 
     }
