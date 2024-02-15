929d9c5d4d443c85bdbef66856ab4c0d06194a76
ganeshrm12 <ganeshmaharanawar@gmail.com>
2024-02-15 19:10:10 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+public class Main{
+
+    public static void main(String [] args){
+
+        System.out.println("HEllo World");
+        System.out.println(new Date());
+        System.out.printn("Heyy");
+        System.out.printn("Heyy123");
+
+    }
+}
