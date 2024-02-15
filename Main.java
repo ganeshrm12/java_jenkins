d8c33770d7e4772b4ba466bb10f9a8de748beaba
ganeshrm12 <ganeshmaharanawar@gmail.com>
2024-02-15 19:10:59 +0530

    initial commit

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ public class Main{
         System.out.println(new Date());
         System.out.printn("Heyy");
         System.out.printn("Heyy123");
+        System.out.printn("Heyy456");
+
 
     }
 }
