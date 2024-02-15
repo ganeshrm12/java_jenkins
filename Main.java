795828d9fab09a2850839e565c00d69ed88d1ad4
ganeshrm12 <ganeshmaharanawar@gmail.com>
2024-02-15 19:45:57 +0530

    user input added

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 import java.util.Date;
+import java.util.*;
 public class Main{
 
     public static void main(String [] args){
+     
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("HEllo World");
         System.out.println(new Date());
         System.out.println("Heyy");
         System.out.println("Heyy123");
         System.out.println("Heyy456");
-
+        System.out.println("Enter name : ");
+        String name = sc.nextLine();
+        System.out.println(name);
 
     }
 }
